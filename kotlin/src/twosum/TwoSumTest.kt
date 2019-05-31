44bfef716b08ecb5880bc629d7b7b5234312b487
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-05-31 23:14:24 +0900

    added leatcode solutions

--- kotlin/src/twosum/TwoSumTest.kt
+++ kotlin/src/twosum/TwoSumTest.kt
@@ -12,15 +12,9 @@ class TwoSumTest {
         twoSum = Solution()
     }
 
-    // OKケース１
-    @Test
-    fun SampleTest001() {
-        var result = twoSum.twoSum(intArrayOf(1), 9)
-        assertEquals(2, result.size)
-    }
 
     @Test
-    fun ExampleTest() {
+    fun TwoSumTest() {
         val nums = intArrayOf(2, 7, 11, 15)
         val target = 9
         var result = twoSum.twoSum(nums, target)
@@ -30,10 +24,52 @@ class TwoSumTest {
         assertEquals(1, result[1])
     }
     @Test
-    fun ExampleTest2() {
+    fun TwoSumTest2() {
         val nums = intArrayOf(3, 2, 3)
         val target = 6
         var result = twoSum.twoSum(nums, target)
+
+        assertEquals(2, result.size)
+        assertEquals(0, result[0])
+        assertEquals(2, result[1])
+    }
+
+    @Test
+    fun TwoSum2Test() {
+        val nums = intArrayOf(2, 7, 11, 15)
+        val target = 9
+        var result = twoSum.twoSum2(nums, target)
+
+        assertEquals(2, result.size)
+        assertEquals(0, result[0])
+        assertEquals(1, result[1])
+    }
+    @Test
+    fun TwoSum2Test2() {
+        val nums = intArrayOf(3, 2, 3)
+        val target = 6
+        var result = twoSum.twoSum2(nums, target)
+
+        assertEquals(2, result.size)
+        assertEquals(0, result[0])
+        assertEquals(2, result[1])
+    }
+
+    @Test
+    fun TwoSum3Test() {
+        val nums = intArrayOf(2, 7, 11, 15)
+        val target = 9
+        var result = twoSum.twoSum3(nums, target)
+
+        assertEquals(2, result.size)
+        assertEquals(0, result[0])
+        assertEquals(1, result[1])
+    }
+    @Test
+    fun TwoSum3Test2() {
+        val nums = intArrayOf(3, 2, 3)
+        val target = 6
+        var result = twoSum.twoSum3(nums, target)
 
         assertEquals(2, result.size)
         assertEquals(0, result[0])
--- kotlin/src/twosum/main.kt
+++ kotlin/src/twosum/main.kt
@@ -1,6 +1,7 @@
 package twosum
 
 import java.lang.IllegalStateException
+import java.util.HashMap
 
 fun main() {
 
@@ -36,5 +37,34 @@ class Solution {
 
         }
         return intArrayOf(resultA, resultB)
+    }
+
+    // https://leetcode.com/problems/two-sum/solution/
+    // これはLeatCodeの解 Approach 2: Two-pass Hash Table
+    fun twoSum2(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int, Int>()
+        for (i in nums.indices) {
+            map[nums[i]] = i
+        }
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement) && map[complement] != i) {
+                return intArrayOf(i, map[complement]!!)
+            }
+        }
+        throw IllegalArgumentException("No two sum solution")
+    }
+
+    // これはLeatCodeの解 Approach 3: One-pass Hash Table
+    fun twoSum3(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int, Int>()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
+            }
+            map[nums[i]] = i
+        }
+        throw IllegalArgumentException("No two sum solution")
     }
 }
