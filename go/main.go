466c87b71ef5aeafde56d55cd35eff541a95761e
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-06-03 01:07:31 +0900

    go

---
+++ go/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("test")
+
+	//head := &ListNode{3, &ListNode{2, &ListNode{0, &ListNode{-4, nil}}}}
+	//
+	//m := map[*ListNode]bool{}
+	//for head.Next != nil {
+	//
+	//	if m[head] {
+	//		return true
+	//	} else  {
+	//		m[head] = true
+	//	}
+	//	head = head.Next
+	//}
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	m := map[*ListNode]bool{}
+	for head.Next != nil {
+
+		if m[head] {
+			return true
+		} else {
+			m[head] = true
+		}
+		head = head.Next
+	}
+}
