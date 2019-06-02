362e3613b28afde464afdde40423d0e0e7924630
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-06-03 00:20:18 +0900

    解

--- kotlin/leatcodekotlin/src/test/kotlin/BestTimetoBuyandSellStock/MaxProfitTest.kt
+++ kotlin/leatcodekotlin/src/test/kotlin/BestTimetoBuyandSellStock/MaxProfitTest.kt
@@ -1,17 +1,24 @@
 package BestTimetoBuyandSellStock
 
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
 class MaxProfitTest {
 
+    private lateinit var target: Solution
+
     @Before
     fun setup() {
 
+        target = Solution()
     }
 
     @Test
     fun maxProfitTest1() {
+        val prices = intArrayOf(7, 1, 5, 3, 6, 4)
+        val result = target.maxProfit(prices)
+        assertEquals(5, result)
 
     }
 }
