3d54e58d6886ede320b08bd3b71cf4ebb8029811
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-06-02 23:43:11 +0900

    maxProfitの準備

---
+++ kotlin/leatcodekotlin/src/test/kotlin/BestTimetoBuyandSellStock/MaxProfitTest.kt
@@ -0,0 +1,17 @@
+package BestTimetoBuyandSellStock
+
+import org.junit.Before
+import org.junit.Test
+
+class MaxProfitTest {
+
+    @Before
+    fun setup() {
+
+    }
+
+    @Test
+    fun maxProfitTest1() {
+
+    }
+}
