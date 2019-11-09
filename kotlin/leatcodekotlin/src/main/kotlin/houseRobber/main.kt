14404ec13d736657ef28e1040a32f23cacbc3544
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-11-09 12:09:46 +0900

    houserobber

--- kotlin/leatcodekotlin/src/main/kotlin/houseRobber/main.kt
+++ kotlin/leatcodekotlin/src/main/kotlin/houseRobber/main.kt
@@ -1,7 +1,42 @@
 package houseRobber
 
+import kotlin.math.abs
+import kotlin.math.max
+
 class Solution {
+    private val dp = arrayListOf<Int>()
+
     fun rob(nums: IntArray): Int {
-        return 1
+        if (nums.isEmpty()) return 0
+        if (nums.size == 1) return nums[0]
+
+        dp.add(nums[0])
+        dp.add(max(nums[0], nums[1]))
+
+        for (i in 2 until nums.size) {
+            dp.add(max(dp[i - 2] + nums[i], dp[i - 1]))
+        }
+        return dp[nums.size - 1]
     }
+
+    fun min(a: Int, b: Int): Int = if (a < b) {
+        a
+    } else {
+        b
+    }
+
+//    fun max(a: Int, b: Int): Int = if (a > b) {
+//        a
+//    } else b
 }
+
+// 1,2,3,1
+// dp[0] = max(nums[0] + nums[0+2], nums[1] + nums[1+2])
+
+
+// 2,7,9,3,1
+// dp[0] = max(nums[0] + nums[0+2], nums[1] + nums[1+2]) // 11
+
+// 2,9,7,3,1
+// dp[0] = max(nums[0] + nums[0+2], nums[1] + nums[1+2]) // 12
+
--- kotlin/leatcodekotlin/src/test/kotlin/houseRobber/houseRobberTest.kt
+++ kotlin/leatcodekotlin/src/test/kotlin/houseRobber/houseRobberTest.kt
@@ -1,4 +1,38 @@
 package houseRobber
 
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
 class HouseRobberTest {
+
+// [1,2,3,1]
+// 4
+
+
+    private lateinit var target: Solution
+    @Before
+    fun before() {
+        target = Solution()
+    }
+
+    @Test
+    fun rob() {
+        val houses = listOf(2, 7, 9, 3, 1)
+
+        val result = target.rob(houses.toIntArray())
+
+//        assertThat(result).isEqualTo(Int.MIN_VALUE)
+        assertThat(result).isEqualTo(12)
+//        assertThat(result).isEqualTo(4)
+    }
+
+    @Test
+    fun rob2() {
+        val houses = listOf(1, 2, 3, 1)
+
+        val result = target.rob(houses.toIntArray())
+        assertThat(result).isEqualTo(4)
+    }
+
 }
