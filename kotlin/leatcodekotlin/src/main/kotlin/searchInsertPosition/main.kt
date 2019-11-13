ba26dc7d3435a3b37afd3e33830b15d10a67d2e5
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-11-13 22:13:00 +0900

    binary search

--- kotlin/leatcodekotlin/src/main/kotlin/searchInsertPosition/main.kt
+++ kotlin/leatcodekotlin/src/main/kotlin/searchInsertPosition/main.kt
@@ -2,6 +2,20 @@ package searchInsertPosition
 
 class Solution {
     fun searchInsert(nums: IntArray, target: Int): Int {
-        return 2
+//        for (index in nums.indices) {
+//            if (target <= nums[index]) return index
+//        }
+//        return nums.size
+
+        var low = 0
+        var high = nums.size - 1
+
+        while (low <= high) {
+            val mid = (low + high) / 2
+            if (nums[mid] == target) return mid
+            else if (nums[mid] > target) high = mid - 1
+            else low = mid + 1
+        }
+        return low
     }
 }
--- kotlin/leatcodekotlin/src/test/kotlin/searchInsertPosition/SearchInsertPositionTest.kt
+++ kotlin/leatcodekotlin/src/test/kotlin/searchInsertPosition/SearchInsertPositionTest.kt
@@ -1,14 +1,21 @@
 package searchInsertPosition
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 
 class SearchInsertPositionTest {
+    private lateinit var target : Solution
 
+    @Before
+    fun setup() {
+        target = Solution()
+    }
     @Test
     fun searchInsert() {
-        val actual = 1
-        assertThat(actual).isEqualTo(1)
+        val actual = target.searchInsert(intArrayOf(1,3,5,6), 5)
+
+        assertThat(actual).isEqualTo(2)
     }
 }
