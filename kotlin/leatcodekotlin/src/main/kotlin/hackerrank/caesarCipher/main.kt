ba4295ffa5592a08be6728ff32bf4a1c625a040a
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-01-01 19:18:00 +0900

    change ide settings

---
+++ kotlin/leatcodekotlin/src/main/kotlin/hackerrank/caesarCipher/main.kt
@@ -0,0 +1,14 @@
+package caesarCipher
+
+import java.util.*
+
+//https://www.hackerrank.com/challenges/caesar-cipher-1/problem
+
+class Solution {
+    // Complete the caesarCipher function below.
+    fun caesarCipher(s: String, k: Int): String {
+
+
+    }
+}
+
---
+++ kotlin/leatcodekotlin/src/test/kotlin/hackerrank/caesarCipher/SolutionTest.kt
@@ -0,0 +1,22 @@
+package caesarCipher
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SolutionTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun caesarCipher() {
+    }
+}
