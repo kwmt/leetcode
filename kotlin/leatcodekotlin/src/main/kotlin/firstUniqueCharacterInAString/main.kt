d6725dbf091ef4dbd495cf424cfbaea233a3ed8f
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-07-22 12:40:07 +0900

    firstUniqueCharacterInAString

---
+++ kotlin/leatcodekotlin/src/main/kotlin/firstUniqueCharacterInAString/main.kt
@@ -0,0 +1,31 @@
+package firstUniqueCharacterInAString
+
+class Solution {
+    data class CounterWithIndex(
+        val counter: Int,
+        val index: Int
+    )
+
+    fun firstUniqChar(s: String): Int {
+        val map = hashMapOf<Char, CounterWithIndex>()
+
+        var i = 0
+        for (c in s) {
+            val data: CounterWithIndex? = map[c]
+
+            if (data != null) {
+                map[c] = data.copy(counter = data.counter + 1)
+                i++
+                continue
+            }
+
+            map[c] = CounterWithIndex(1, i)
+            i++
+        }
+        // 1より大きいものは考慮しない
+        // 1よ理大きいものを除いたものなかから、indexが最小のものを返す。
+        // 見つからなかったら -1を返す。
+        return map.filterNot { it.value.counter > 1 }.values.minBy { it.index }?.index ?: -1
+
+    }
+}
---
+++ kotlin/leatcodekotlin/src/test/kotlin/firstUniqueCharacterInAString/SolutionTest.kt
@@ -0,0 +1,27 @@
+package firstUniqueCharacterInAString
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+
+class SolutionTest {
+
+    lateinit var target : Solution
+    @Before
+    fun setUp() {
+        target = Solution()
+    }
+
+    @Test
+    fun firstUniqChar() {
+        val actual = target.firstUniqChar("leetcode")
+        assertThat(actual).isEqualTo(0)
+        val actual2 = target.firstUniqChar("loveleetcode")
+        assertThat(actual2).isEqualTo(2)
+        val actual3 = target.firstUniqChar("aabb")
+        assertThat(actual3).isEqualTo(-1)
+        val actual4 = target.firstUniqChar("dddccdbba")
+        assertThat(actual4).isEqualTo(8)
+    }
+}
